c619d8eaefadf4fefb8641fbfa0214ce5df4f6b6
CREEK <41039917+creekdrops@users.noreply.github.com>
2023-12-11 15:10:50 -0600

    updated to only run on create and noted bug

--- src/collections/Employees/hooks.ts
+++ src/collections/Employees/hooks.ts
@@ -8,35 +8,30 @@ export const addToSortedHook: CollectionAfterChangeHook = async ({
   operation,
   req,
 }) => {
-  if (operation === "create" || (operation === "update" && doc)) {
-    const { id } = doc;
+  // Only run this hook when creating a new employee
+  if (operation === "create" && doc) {
+    try {
+      const { id } = doc;
 
-    const employeeId = parseInt(id);
+      const employeeId = parseInt(id);
 
-    const { employeeOrder } = await req.payload.findGlobal({
-      slug: "sorting",
-      depth: 0,
-    });
-    if (
-      !employeeOrder.some(
-        (item) =>
-          typeof item.employee === "number" && item.employee === employeeId
-      )
-    ) {
-      try {
-        console.log("Adding employee");
-        console.log("employeeId", employeeId);
-        await req.payload.updateGlobal({
-          slug: "sorting",
-          data: {
-            employeeOrder: [...employeeOrder, { employee: employeeId }],
-          },
-          overrideAccess: true,
-        });
-        console.log("Employee added");
-      } catch (error) {
-        console.log(error);
-      }
+      const { employeeOrder } = await req.payload.findGlobal({
+        slug: "sorting",
+        depth: 0,
+      });
+
+      // BUG: It currently appears that the employees table has not been updated with
+      // the new employee record at the time this hooks is run.
+
+      await req.payload.updateGlobal({
+        slug: "sorting",
+        data: {
+          employeeOrder: [...employeeOrder, employeeId],
+        },
+        overrideAccess: true,
+      });
+    } catch (error) {
+      console.log(error);
     }
   }
 };
